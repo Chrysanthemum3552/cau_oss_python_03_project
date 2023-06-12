526c4f1d4e388f797ce51c226065ba69a0024f99
SeungJong Lee <chrysanthemum3552@gmail.com>
2023-06-12 10:17:39 +0900

    version #2

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,20 @@
+
+
 def start_process(path):
+
+    """
+        1. file_manager의 read_file함수를 사용하므로 file_manager을 import한다.
+        2. parking_spot_manager의 str_list_to_class_list함수를 사용하므로 parking_spot_manager도 import한다.
+        3. 매개변수로 받은 path를 read_file함수에 대입하여 반환된 목록들을 str_list에 저장한다.
+        4. str_list를 parking_spot객체의 리스트로 반환받기 위해  함수 str_list_to_class_list에 str_list를 대입한다.
+        5. 이를 spots에 저장한다.
+    """
+
+    import file_manager
+    import parking_spot_manager
+    str_list=file_manager.read_file(path)
+    spots=parking_spot_manager.str_list_to_class_list(str_list)
+
     while True:
         print("---menu---")
         print("[1] print")
@@ -7,8 +23,12 @@ def start_process(path):
         print("[4] exit")
         select = int(input('type:'))
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            """
+                1번 옵션을 선택하면 parking_spot_manager 모듈의 
+                print_spots 함수를 호출해야 하므로 parking_spot_manager.print_spots() 
+                함수에 위에서 구한 spots를 대입하여 목록을 전부 print한다.
+            """           
+            parking_spot_manager.print_spots(spots) 
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -52,7 +72,10 @@ def start_process(path):
                 # fill this block
             else: print("invalid input")
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            """
+                4를 입력하면 Exit을 표시한뒤 break를 통해 반복문을 벗어나 프로그램을 종료한다.
+            """
+            print("Exit")
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,6 +1,37 @@
-class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
+class parking_spot:  
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        """
+            parking_spot의 생성자.
+            매개변수의 종류는 아래와 같다
+                name : 자원명
+                city : 시도
+                district : 시군구
+                ptype : 주차장유형
+                longitude : 경도
+                latitude : 위도
+                  
+            이를 딕셔너리 __item 필드의 value에 대입한다.
+            따라서 키(key)와 값(value)은 아래와 같이 구성된다.  
+                name : 자원명
+                city : 시도
+                district : 시군구
+                ptype : 주차장유형
+                longitude : 경도
+                latitude : 위도    
+        """
+        
+        self.__item = {
+
+            'name'      : name,
+            'city'      : city,
+            'district'  : district,
+            'ptype'     : ptype,
+            'longitude' : float(longitude),
+            'latitude'  : float(latitude)
+        }
+    
     def __str__(self):
+        
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
@@ -8,12 +39,59 @@ class parking_spot:
         return s
 
 
+    def get(self, keyword='name'):
+        """
+            매개변수 keyword를 받아 __item 딕셔너리의 __item[keyword]를 반환한다.
+            ※ 입력이 없을시 기본인수는 'name'이 된다.
+        """
+        return self.__item[keyword]
+
+    
+def str_list_to_class_list(str_list):
+    """
+        1. str_list를 매개변수로 받는다.
+        2. 이를 split을 이용해 콤마(,)단위로 쪼개서 리스트 data_list에 각각 대입한다.
+        3. 순번은 data_list[0]에 저장되는데 이는 요구하지 않으므로 1번부터 각각의 변수에 대입한다.
+        4. 이를 클래스 객체 리스트로 변환하기 위해 클래스 parking_spot에 대입한다.
+        5. 이를 spot에 저장하고 이를 append를 이용하여 class_list에 추가한다.
+        6. for의 모든 과정이 끝나면 class_list를 반환한다.
+    """
+    class_list=[]
+    for item in str_list:
+        data_list=item.split(',')
+        name = data_list[1]
+        city = data_list[2]
+        district = data_list[3]
+        ptype = data_list[4]
+        longitude = float(data_list[5])
+        latitude = float(data_list[6])
+        spot = parking_spot(name, city, district, ptype, longitude, latitude)
+        class_list.append(spot)
+    return  class_list
+
+
+
+def print_spots(spots):
+    """
+        class_lits를 저장한 값을 받아 매개변수 spots에 저장한다.
+        그 다음 len함수를 이용하여 elements의 개수를 표시한뒤 spots의 모든 데이터들을 표시한다.
+    """
+    print(f"---print elements({len(spots)})---")
+    for data in spots:
+        print(str(data))
+
+
+
+
+    
+
+
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
-    # version#2
+# version#2
     # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
+    # str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
     # spots = str_list_to_class_list(str_list)
     # print_spots(spots)
 
